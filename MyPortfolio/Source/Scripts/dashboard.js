bbec863eac0719154e586d77bd653b9657b29868
nae deshazer <naedeshazer@naes-MBP.grandenetworks.net>
2019-05-24 21:54:35 -0500

    dashboard

--- MyPortfolio/Source/Scripts/dashboard.js
+++ MyPortfolio/Source/Scripts/dashboard.js
@@ -79,3 +79,22 @@ function weather() {
 }
 
 weather();
+
+// this is for the geolocation //
+var x = document.getElementById("one");
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		x.innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+
+function showPosition(position) {
+	x.innerHTML =
+		"Latitude: " +
+		position.coords.latitude +
+		"<br>Longitude: " +
+		position.coords.longitude;
+}
