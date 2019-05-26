1537e8c41e4cd946c49bf41411bea3fe1a620eec
nae deshazer <naedeshazer@naes-MBP.grandenetworks.net>
2019-05-26 09:51:36 -0500

    tower

--- MyPortfolio/Source/Scripts/dashboard.js
+++ MyPortfolio/Source/Scripts/dashboard.js
@@ -1,3 +1,22 @@
+//this is for the to do list //
+
+$(function() {
+	let $list = $("ul");
+	let $newItemForm = $("#newItemForm");
+
+	$newItemForm.on("submit", function(e) {
+		e.preventDefault();
+		let text = $('input[type="text"]').val();
+		$list.append(`<li>${text}</li>`);
+		$('input[type="text"]').val("");
+	});
+
+	$list.on("click", "li", function() {
+		let $this = $(this);
+		$this.remove();
+	});
+});
+
 // this is foe the date and time //
 
 function startTime() {
